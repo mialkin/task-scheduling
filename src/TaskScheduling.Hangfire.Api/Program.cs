b27e933ebe777bb9b809c5e034a41dc5b8ffa05b
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-08 02:21:00 +0300

    Setup Hangfire

--- src/TaskScheduling.Hangfire.Api/Program.cs
+++ src/TaskScheduling.Hangfire.Api/Program.cs
@@ -1,6 +1,23 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+
+// Add the processing server as IHostedService
+services.AddHangfireServer();
+
 var application = builder.Build();
 
-application.MapGet("/", () => "Hello World!");
+application.UseHangfireDashboard("", new DashboardOptions
+{
+    DarkModeEnabled = false
+});
 
 application.Run();
---
+++ src/TaskScheduling.Hangfire.Api/readme.md
@@ -0,0 +1,16 @@
+# Hangfire
+
+1\. Run SQL Server:
+
+```bash
+docker-compose up
+```
+
+2\. Create database:
+
+```sql
+CREATE DATABASE [HangfireTest]
+GO
+```
+
+3\. Run application and navigate to <http://localhost:5180>.
